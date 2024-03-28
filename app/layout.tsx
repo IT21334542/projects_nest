9bc8552aa8248b1a1dd3cfb9c485e3727191fead
Sanjeevan <it21334542@my.sliit.lk>
2024-03-28 17:50:52 +0530

    wrapped the application to get use the useSession hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import { Theme } from "@radix-ui/themes";
+import AuthProvider from "./auth/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>
-          <main>{children}</main>
-        </Theme>
+        <AuthProvider>
+          <Theme>
+            <main>{children}</main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
