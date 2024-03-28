e2e169ab55f13eaafe00a24dc39ac5d20a891065
Sanjeevan <it21334542@my.sliit.lk>
2024-03-28 18:33:57 +0530

    configured middle warefor masters page

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import {withAuth} from "next-auth/middleware"
+
+export default withAuth({
+   callbacks:{
+    authorized: async ({req,token})=>{
+
+        if(req.nextUrl.pathname.startsWith('/master'))
+            return (token?.isMaster==true)
+        return true;
+    }
+   }
+})
+
+export const config = {
+    matcher:['/admin/:path*']
+}
