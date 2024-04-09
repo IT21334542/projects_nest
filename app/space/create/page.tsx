0ce427f8f2b260cf129d965c29eda5e2dfb55050
Sanjeevan <it21334542@my.sliit.lk>
2024-04-10 01:43:05 +0530

    refined Production Bug #1

--- app/space/create/page.tsx
+++ app/space/create/page.tsx
@@ -33,7 +33,7 @@ const SpaceCreatePage = () => {
   const [User, SetUser] = useState<string | null>(null);
 
   useEffect(() => {
-    if (User) SetUser(User);
+    if (!User) SetUser(User);
   }, [data, User]);
 
   const {
