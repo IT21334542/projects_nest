bddf51c7f8ea2c4722178a888543608d3c665752
Sanjeevan <it21334542@my.sliit.lk>
2024-04-11 17:28:03 +0530

    Space create button not visible

--- app/space/create/page.tsx
+++ app/space/create/page.tsx
@@ -64,74 +64,76 @@ const SpaceCreatePage = () => {
   return (
     <Flex className=" bg-[#292A2C] w-screen h-screen" direction={"column"}>
       <form onSubmit={Handling}>
-        <Grid columns={"2"} height={"100%"}>
-          <Box p={"6"}>
-            <Link href={"/space/all"}>
-              <FiArrowLeft
-                color="#ffffff"
-                size={30}
-                className=" border border-white px-0.5 py-1 mb-6"
-              />
-            </Link>
-            <Text className=" text-white text-2xl">Create new WorkSpace</Text>
-            <br />
-            <Box className=" mt-9">
-              <Text weight={"light"} className=" text-white text-xl ">
-                Space name
-              </Text>
-              <TextField.Root
-                placeholder=" Space Name"
-                className=" max-w-lg mt-2"
-                {...register("name", {
-                  required: "Name is Required",
-                  minLength: 1,
-                })}
-              />
-            </Box>
+        {User && (
+          <Grid columns={"2"} height={"100%"}>
+            <Box p={"6"}>
+              <Link href={"/space/all"}>
+                <FiArrowLeft
+                  color="#ffffff"
+                  size={30}
+                  className=" border border-white px-0.5 py-1 mb-6"
+                />
+              </Link>
+              <Text className=" text-white text-2xl">Create new WorkSpace</Text>
+              <br />
+              <Box className=" mt-9">
+                <Text weight={"light"} className=" text-white text-xl ">
+                  Space name
+                </Text>
+                <TextField.Root
+                  placeholder=" Space Name"
+                  className=" max-w-lg mt-2"
+                  {...register("name", {
+                    required: "Name is Required",
+                    minLength: 1,
+                  })}
+                />
+              </Box>
 
-            {errors.name && (
-              <Callout.Root variant="surface" color="red" m={"1"}>
-                <Callout.Icon>
-                  <FiInfo color="#ffffff" />
-                </Callout.Icon>
-                <Callout.Text color="red">
-                  <Text className=" text-white">{errors.name.message}</Text>
-                </Callout.Text>
-              </Callout.Root>
-            )}
+              {errors.name && (
+                <Callout.Root variant="surface" color="red" m={"1"}>
+                  <Callout.Icon>
+                    <FiInfo color="#ffffff" />
+                  </Callout.Icon>
+                  <Callout.Text color="red">
+                    <Text className=" text-white">{errors.name.message}</Text>
+                  </Callout.Text>
+                </Callout.Root>
+              )}
 
-            <Box className=" mt-9">
-              <Text weight={"light"} className=" text-white text-xl ">
-                Description
-              </Text>
-              <TextArea
-                placeholder=" tell some facts about this workspace..."
-                className=" max-w-lg mt-2"
-                {...register("description")}
-              />
+              <Box className=" mt-9">
+                <Text weight={"light"} className=" text-white text-xl ">
+                  Description
+                </Text>
+                <TextArea
+                  placeholder=" tell some facts about this workspace..."
+                  className=" max-w-lg mt-2"
+                  {...register("description")}
+                />
+              </Box>
+              {User && (
+                <Button type="submit" color="brown" mt={"6"}>
+                  <FiPlus color="#ffffff" />
+                  Create Space
+                </Button>
+              )}
             </Box>
-            {User && (
-              <Button type="submit" color="brown" mt={"6"}>
-                <FiPlus color="#ffffff" />
-                Create Space
-              </Button>
-            )}
-          </Box>
-          <Box className=" w-full h-full ">
-            <Flex className=" h-full max-h-screen">
-              <img
-                src="https://images.pexels.com/photos/4348082/pexels-photo-4348082.jpeg?auto=compress&cs=tinysrgb&w=600"
-                style={{
-                  display: "block",
-                  objectFit: "cover",
-                  width: "100%",
-                  height: "100%",
-                  backgroundColor: "var(--gray-5)",
-                }}
-              />
-            </Flex>
-          </Box>
-        </Grid>
+            <Box className=" w-full h-full ">
+              <Flex className=" h-full max-h-screen">
+                <img
+                  src="https://images.pexels.com/photos/4348082/pexels-photo-4348082.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  style={{
+                    display: "block",
+                    objectFit: "cover",
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "var(--gray-5)",
+                  }}
+                />
+              </Flex>
+            </Box>
+          </Grid>
+        )}
       </form>
     </Flex>
   );
